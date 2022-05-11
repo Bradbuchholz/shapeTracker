12a003b529a3932eeebe1ed216424d41d13c602f
Bradbuchholz <bradbuchholz@gmail.com>
2022-05-10 19:53:13 -0700

    test passed

--- src/triangle.js
+++ src/triangle.js
@@ -5,10 +5,13 @@ export default function Triangle(side1, side2, side3) {
   this.isTriangle = ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) ? "not a triangle" : "this is a triangle"; 
 }
 
+
 Triangle.prototype.checkType = function() {
   if ((this.side1 > (this.side2 + this.side3)) || (this.side2 > (this.side1 + this.side3)) || (this.side3 > (this.side1 + this.side2))) {
     return "not a triangle";
   } else if ((this.side1 !== this.side2) && ((this.side1 !== this.side3)) && ((this.side2 !== this.side3))) {
     return "scalene triangle";
-  }
+  }  else if ((this.side1 === this.side2) || ((this.side1 === this.side3)) || ((this.side2 === this.side3))) {
+      return "isosceles triangle";
+    }
 };
